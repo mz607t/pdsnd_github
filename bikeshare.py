2d90f850811616348177b15b579847a8857fbcd8
Marina <marina.marinova@intl.att.com>
2021-01-29 10:02:53 +0200

    Add more comments to code in bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -69,6 +69,7 @@ def load_data(city, month, day):
     df['Start Time'] = pd.to_datetime(df['Start Time'])
 
     #Extract month and day of week from Start Time to create new columns
+    #Add more comments here
     df['month'] = df['Start Time'].dt.month
     df['day_of_week'] = df['Start Time'].dt.weekday_name
 
